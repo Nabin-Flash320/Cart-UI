bf38c5d4f2504e65c20d9305933a6a8cc5b6c670
Nabin-Flash320 <nabindangi32@gmail.com>
2022-03-04 14:36:20 +0545

    Image and cart details added.

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -1,17 +1,38 @@
-# -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ShoppingCart.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.1
-#
-# WARNING! All changes made in this file will be lost!
-
-
+from itertools import count
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtGui import QImage, QPixmap
+import numpy as np
+import cv2 as cv
 import sys
 import requests
 import json
+
+
+class VideoThread(QThread):
+    change_pixmap_signal = pyqtSignal('PyQt_PyObject')
+
+    def __init__(self):
+        super().__init__()
+        self._run_flag = True
+
+    def run(self):
+        # capture from web cam
+        cap = cv.VideoCapture(0)
+        while self._run_flag:
+            ret, cv_img = cap.read()
+            if ret:
+                self.change_pixmap_signal.emit(cv_img)
+        # shut down capture system
+        cap.release()
+
+    def stop(self):
+        """Sets run flag to False and waits for thread to finish"""
+        self._run_flag = False
+        self.wait()       
+
 
 class TableView(QTableWidget):
     def __init__(self, data, *args):
@@ -22,7 +43,6 @@ class TableView(QTableWidget):
         self.resizeRowsToContents()
         self.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
 
- 
     def setData(self): 
         horHeaders = []
         for n, key in enumerate(self.data.keys()):
@@ -36,24 +56,45 @@ class TableView(QTableWidget):
 class ApplyQuery():
     def __init__(self, url):
         self.url = url
+        self.product_id = None
 
     def get_url(self):
         return self.url
+    
+    def query_post(self, id, flag='c', product_id=None):
+        self.product_id = product_id
+        if flag == 'c' and self.product_id:
+            payload = {
+                'product_id':str(self.product_id),
+                'count':'1'
+            }
+            r = requests.post(self.url+'/'+str(id)+'/data', payload)
+            datas = json.loads(r.text)
+            return self.parse_data(datas=datas)
 
     def query_get(self, id, flag='p'):
         if flag=='p':
-            r = requests.get(self.get_url()+'/'+str(id)+'/data')
+            r = requests.get(self.url+'/'+str(id)+'/data')
             datas = json.loads(r.text)
-            data_dict = dict()
-            for data in datas:
-                for key, value in data.items():
-                    if key == 'product_mfd' or key == 'product_epd':
-                        continue
-                    if key in data_dict.keys():
-                        data_dict[key].append(str(value))
-                    else:
-                        data_dict[key] = list([str(value)])
-            return data_dict
+            return self.parse_data(datas=datas)
+
+    def completed(self, id):
+        payload = {
+            'completed': True
+        }
+        r = requests.post(self.url+'/'+str(id)+'/completed', payload)
+    
+    def parse_data(self, datas):
+        data_dict = dict()
+        for data in datas:
+            for key, value in data.items():
+                if key == 'product_mfd' or key == 'product_epd':
+                    continue
+                if key in data_dict.keys():
+                    data_dict[key].append(str(value))
+                else:
+                    data_dict[key] = list([str(value)])
+        return data_dict
 
 
 class Ui_ShoppingCart(object):
@@ -61,7 +102,7 @@ class Ui_ShoppingCart(object):
         ShoppingCart.setObjectName("ShoppingCart")
         ShoppingCart.resize(637, 480)
 
-        query = ApplyQuery('http://192.168.2.72:8080/shop')
+        query = ApplyQuery('http://localhost:8080/shop')
 
 
         self.centralwidget = QtWidgets.QWidget(ShoppingCart)
@@ -81,15 +122,14 @@ class Ui_ShoppingCart(object):
             "    font-size:20px;\n"
             "}")
 
-
+        self.cart_data = query.query_post(id=1, product_id=1)
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 70, 361, 351))
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 70, 300, 351))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.CartData = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.CartData.setContentsMargins(0, 0, 0, 0)
         self.CartData.setObjectName("CartData")
-        # self.CartDataTable = TableView(self.product_data, len(self.product_data[list(self.product_data.keys())[0]]), len(list(self.product_data.keys())), self.horizontalLayoutWidget)
-        self.CartDataTable = QtWidgets.QTableView(self.horizontalLayoutWidget)
+        self.CartDataTable = TableView(self.cart_data, len(self.cart_data[list(self.cart_data.keys())[0]]), len(list(self.cart_data.keys())), self.horizontalLayoutWidget)
         self.CartDataTable.setObjectName("CartDataTable")
         self.CartData.addWidget(self.CartDataTable)
         self.CartDataTable.setStyleSheet("QFrame\n"
@@ -101,29 +141,34 @@ class Ui_ShoppingCart(object):
             "}")
 
 
-
-        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(390, 70, 231, 141))
-        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
-        self.Images = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
-        self.Images.setContentsMargins(0, 0, 0, 0)
-        self.Images.setObjectName("Images")
-        self.PiCameraView = QtWidgets.QGraphicsView(self.horizontalLayoutWidget_2)
-        self.PiCameraView.setObjectName("PiCameraView")
-        self.Images.addWidget(self.PiCameraView)
-        self.PiCameraView.setStyleSheet("QFrame\n"
+        self.imageLabel = QtWidgets.QLabel(self.centralwidget)
+        self.imageLabel.setGeometry(QtCore.QRect(335, 70, 282, 181))
+        self.imageLabel.setObjectName("PiCameraView")
+        self.imageLabel.setStyleSheet("QFrame\n"
             "{\n"
             "    background-color: rgb(255, 255, 255);\n"
             "    \n"
             "    color: rgb(0, 0, 0);\n"
             "    border-radius:10px;\n"
+            "    font-size:20px;\n"
             "}")
+
+
+        # create the video capture thread
+        self.thread = VideoThread()
+        # connect its signal to the update_image slot
+        self.thread.change_pixmap_signal.connect(self.update_image)
+        # start the thread
+        self.thread.start()
+
 
         # Get the product data fromt the database...
         self.product_data = query.query_get(1, 'p')
+        
 
         self.horizontalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_3.setGeometry(QtCore.QRect(390, 220, 231, 251))
+        #(390, 220, 231, 251)
+        self.horizontalLayoutWidget_3.setGeometry(QtCore.QRect(335, 260, 282, 211))
         self.horizontalLayoutWidget_3.setObjectName("horizontalLayoutWidget_3")
         self.AvailableProducts = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_3)
         self.AvailableProducts.setContentsMargins(0, 0, 0, 0)
@@ -131,16 +176,10 @@ class Ui_ShoppingCart(object):
         self.AvailableProductTable = TableView(self.product_data, len(self.product_data[list(self.product_data.keys())[0]]), len(list(self.product_data.keys())), self.horizontalLayoutWidget)
         self.AvailableProductTable.setObjectName("AvailableProductTable")
         self.AvailableProducts.addWidget(self.AvailableProductTable)
-        self.AvailableProductTable.setStyleSheet("QFrame\n"
-            "{\n"
-            "    background-color: rgb(255, 255, 255);\n"
-            "    \n"
-            "    color: rgb(0, 0, 0);\n"
-            "    border-radius:10px;\n"
-            "}")
+
 
         self.SubmitButton = QtWidgets.QPushButton(self.centralwidget)
-        self.SubmitButton.setGeometry(QtCore.QRect(20, 430, 361, 41))
+        self.SubmitButton.setGeometry(QtCore.QRect(20, 430, 300, 41))
         self.SubmitButton.setObjectName("SubmitButton")
 
 
@@ -148,6 +187,27 @@ class Ui_ShoppingCart(object):
 
         self.retranslateUi(ShoppingCart)
         QtCore.QMetaObject.connectSlotsByName(ShoppingCart)
+
+
+    def update_image(self, cv_img):
+        """Updates the image_label with a new opencv image"""
+        qt_img = self.convert_cv_qt(cv_img)
+        self.imageLabel.setPixmap(qt_img)
+    
+    def convert_cv_qt(self, cv_img):
+        """Convert from an opencv image to QPixmap"""
+        rgb_image = cv.cvtColor(cv_img, cv.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(282, 211, QtCore.Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+
+
+    def closeEvent(self, event):
+        self.thread.stop()
+        event.accept()
+
 
     def retranslateUi(self, ShoppingCart):
         _translate = QtCore.QCoreApplication.translate
